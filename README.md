631365e871b64d53a41cfdcb4e04fdd14c5e9d4d
= <=>
2019-07-17 15:32:46 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# NewTabBookmarks
+Bookmark on new tab page made by @duc1607.
+
+This ultilizes the browser's localStorage.
+
+## Installation
+No need, just run index.html and you are good to go!
+
+## Chrome extension
+You can install this as your Chrome's new tab page with ease
+1. Enable developer mode
+2. Clone this git and unzip
+3. Click "Load unpacked" and select the unzipped folder
---
+++ scripts.js
@@ -0,0 +1,69 @@
+$(function() {
+	// initiate localStorage if undefined
+	if (localStorage.links == undefined) {
+		localStorage.links = "[]";
+	}
+
+	// get links from localStorage
+	let links = JSON.parse(localStorage.links);
+
+	// iterate and prepend into the main div
+	$.each(links, function (k, v) {
+		let id = links[k]['id'];
+		let icon = links[k]['icon'];
+		let link = links[k]['link'];
+		let name = links[k]['name'];
+		addElem(id,icon, link, name);
+	});
+
+	//add event listener for the Add button
+	$('.addBtn').click(function () {
+		const pattern = /((https?:\/\/)?[a-zA-Z0-9.]+)/gm;
+
+		let name = $('#name').val();
+		let link = $('#link').val();
+
+		if (name.length > 0 && link.length > 0) {
+			// get favicon based on most website's links
+			let icon = link.match(pattern)[0] + "/favicon.ico";
+
+			//get a random ID for this element
+			let id = Math.random().toString(36).slice(2);
+
+			let links = JSON.parse(localStorage.links);
+			links.push({"id":id, "name":name, "link":link, "icon":icon});
+			localStorage.links = JSON.stringify(links);
+			
+			addElem(id,icon, link, name);
+		}
+	});
+
+	//add event listener for the Delete button
+	$('.deleteElem').click(function(e) {
+		e.preventDefault();
+
+		let id = $(this).attr('elem-id');
+
+		let links = JSON.parse(localStorage.links);
+		links.pop({"id": id});
+		localStorage.links = JSON.stringify(links);
+
+		$('div [elem-id='+id+']').parent().css('transform','scale(0)');
+		setTimeout(function() {
+			$('div [elem-id=' + id + ']').parent().remove();
+		},250);
+	});
+});
+
+function addElem(id,icon,link,name) {
+	var element = '<a href="'+link+'">\
+					<div class="element">\
+						<img src="'+icon+'" class="siteIcon">\
+						<div>\
+							<h3>'+name+'</h3>\
+						</div>\
+						<a href="" class="deleteElem" elem-id="'+ id +'"></a>\
+					</div>\
+					</a>';
+	$('#main').prepend(element);
+}
